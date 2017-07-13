75d7d3d9361861a596019335106b0577a1aee725
huqi <59943915@qq.com>
2017-07-13 17:39:29 +0800

    yi

--- app/src/main/java/com/example/helloworld/HelloWorldActivity.java
+++ app/src/main/java/com/example/helloworld/HelloWorldActivity.java
@@ -12,6 +12,7 @@ public class HelloWorldActivity extends AppCompatActivity {
         setContentView(R.layout.hello_world_layout);
         //test git
         //test git2
+        //一二三
       //  Log.d("HelloWorldActivity","onCreate execute");
         Log.d("data","onCreate execute");
 
